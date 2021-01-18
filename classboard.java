2cce7f5acdd15665e24f37a5b9e0f1bafa539965
lekipising <leeplanke@gmail.com>
2021-01-18 16:01:02 +0200

    Monday 18 jan-2

--- classboard.java
+++ classboard.java
@@ -1,39 +1,43 @@
 public class classboard {
-    static boolean checkpalindrome(String word){
-        int size = word.length();
+    static void checkpalindrome(String word){
+        int size = word.length();  // get the size of the string
 
-        int search = (size - 1) / 2;
+        int search = (size - 1) / 2;  // Number of times to search/compare
 
-        int m = 0;
-        int n = size - 1;
+        int m = 0;  // Index of same char to the left of middle char
+        int n = size - 1;  // Index of same char to the right of middle char
 
+        while (search > 0){
+            char x = word.charAt(m);  // get the left char
+            char y = word.charAt(n);  // get the right char
 
-        if (size %2 == 0){
-            return false;
-        }
-        else{
-            while (search > 0){
-                char x = word.charAt(m);
-                char y = word.charAt(n);
-
-                if (x == y){
-                    search --;
-                    if (search == 0){
-                        return true;
-                    }
+            if (x == y){  // check if they are same
+                search --;
+                if (search == 0){   // continue searching if number of searches are not 0
+                    System.out.println("A palindrome"); 
                 }
-                else{
-                    return false;
-                }
-                
-
+            }
+            else{  // 
+                System.out.println("Not a palindrome");
+                break;
             }
 
+            m ++;
+            n --;
 
+            // if (m == n){
+                
+            // }
 
         }
+
     }
     public static void main(String[] args){
-        System.out.println(checkpalindrome("liplan"));
+        checkpalindrome("12821");
     }
 }
+
+
+// Not considered yet (inputs):
+// - One character
+// - Wrong input -- integers, etc
